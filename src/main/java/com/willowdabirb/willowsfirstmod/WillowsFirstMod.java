6a5d05f756f2625b011c507907ce9b2627e43b53
willowdabirb <willowdabirb@gmail.com>
2026-04-03 14:37:09 -0700

    Added Peridot and Block of Peridot

--- src/main/java/com/willowdabirb/willowsfirstmod/WillowsFirstMod.java
+++ src/main/java/com/willowdabirb/willowsfirstmod/WillowsFirstMod.java
@@ -1,5 +1,8 @@
 package com.willowdabirb.willowsfirstmod;
 
+import com.willowdabirb.willowsfirstmod.block.ModBlocks;
+import com.willowdabirb.willowsfirstmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +37,9 @@ public class WillowsFirstMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -47,6 +53,12 @@ public class WillowsFirstMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.PERIDOT);
+        }
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.PERIDOT_BLOCK);
+        }
 
     }
 
---
+++ src/main/java/com/willowdabirb/willowsfirstmod/item/ModItems.java
@@ -0,0 +1,18 @@
+package com.willowdabirb.willowsfirstmod.item;
+
+import com.willowdabirb.willowsfirstmod.WillowsFirstMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(WillowsFirstMod.MOD_ID);
+
+    public static final DeferredItem<Item> PERIDOT = ITEMS.register("peridot",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
